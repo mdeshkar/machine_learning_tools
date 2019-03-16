d93d81c7bb40210ddd9d2baddf7794ce9b0bc990
Mandar Deshkar <35677643+mdeshkar@users.noreply.github.com>
2019-03-15 21:51:39 -0700

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -1,10 +1,35 @@
 # Data Processing
 
-# Importing the Libraries
+# Importing the dataset
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+dataset = read.csv('Data.csv')
+# dataset = dataset[,2:3]
 
-# Importing the data set
-print("Mandar")
+
+# Identifying the missing values
+# dataset$Age = ifelse(is.na(dataset$Age),ave(dataset$Age,FUN = function(x)mean(x,na.rm=TRUE)),
+#                      dataset$Age)
+# 
+# dataset$Salary = ifelse(is.na(dataset$Salary),ave(dataset$Salary,FUN = function(x)mean(x,na.rm = T))
+#                         ,dataset$Salary)
+
+# Encoding categorical data
+
+# dataset$Country = factor(dataset$Country,
+#                          levels = c('France','Spain','Germany'),
+#                          labels = c(1,2,3))
+# dataset$Purchased = factor(dataset$Purchased,
+#                            levels = c('No','Yes')
+#                            ,labels = c(0,1))
+# Spliting the dataset into the training set and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio = 0.8)
+training_set = subset(dataset,split==T)
+testing_set = subset(dataset,split==F)
+
+# Feature Scalling
+# training_set[,c(2,3)] = scale(training_set[,c(2,3)])
+# testing_set[,c(2,3)] = scale(testing_set[,c(2,3)])
+
